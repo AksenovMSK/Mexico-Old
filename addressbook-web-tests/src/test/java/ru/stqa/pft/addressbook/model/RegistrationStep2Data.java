9fe7a5aa29fd49bf445105a9b28fd7e9ba30a746
AksenovMSK <44177189+AksenovMSK@users.noreply.github.com>
2018-10-30 17:44:01 +0300

    добавил вторую модель для регистрации

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/RegistrationStep2Data.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/RegistrationStep2Data.java
@@ -1,4 +1,28 @@
 package ru.stqa.pft.addressbook.model;
 
 public class RegistrationStep2Data {
+    private final String smsCode;
+    private final String password;
+    private final String confirmationPassword;
+
+    public RegistrationStep2Data (String smsCode,
+                                  String password,
+                                  String confirmationPassword){
+
+        this.smsCode = smsCode;
+        this.password = password;
+        this.confirmationPassword = confirmationPassword;
+    }
+
+    public String getSmsCode() {
+        return smsCode;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getConfirmationPassword() {
+        return confirmationPassword;
+    }
 }
