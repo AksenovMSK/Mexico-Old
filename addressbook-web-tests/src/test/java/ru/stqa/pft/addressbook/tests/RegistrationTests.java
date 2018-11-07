b30b4d95314e45c9552f043e460bd63fbcc2c7a9
AksenovMSK <44177189+AksenovMSK@users.noreply.github.com>
2018-11-07 15:50:26 +0300

    закоментировал второй шаг регистрации

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/RegistrationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/RegistrationTests.java
@@ -15,15 +15,15 @@ public class RegistrationTests extends TestBase  {
                 "TestFathername",
                 "TestMothername",
                 "test@mail.ru"), new GenerationData());
-        app.getRegistrationHelper().selectBirthPlace(); //пустой
+        app.getRegistrationHelper().selectBirthPlace();
         app.getRegistrationHelper().agreeCookies();
         app.getRegistrationHelper().agreePersonal();
         app.getRegistrationHelper().submitRegistrationForm1();
-        app.getRegistrationHelper().fillRegistrationForm2(new BasicRegistrationData(
-                "111111",
-                "123456q",
-                "123456q"));
-        app.getRegistrationHelper().submitRegistrationForm2();
+//        app.getRegistrationHelper().fillRegistrationForm2(new BasicRegistrationData(
+//                "111111",
+//                "123456q",
+//                "123456q"));
+//        app.getRegistrationHelper().submitRegistrationForm2();
     }
 
 }
