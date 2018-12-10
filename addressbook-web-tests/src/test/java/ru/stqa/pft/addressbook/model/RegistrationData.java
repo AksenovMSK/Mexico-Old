75029b2f9639d2c39ce9f2f7a540d17970bf2c22
AksenovMSK <44177189+AksenovMSK@users.noreply.github.com>
2018-12-10 14:38:57 +0300

    добавил в класс с данными поля с 1 шага ОТ

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/RegistrationData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/RegistrationData.java
@@ -10,6 +10,18 @@ public class RegistrationData {
     private String email;
     private String smsCode;
     private String password;
+    private String curp;
+    private String rfc;
+    private String flat;
+    private String house;
+    private String street;
+    private String postIndex;
+    private String colony;
+    private String State;
+    private String delegation;
+
+
+
 
     public RegistrationData withFirstName(String firstName) {
         this.firstName = firstName;
@@ -51,6 +63,50 @@ public class RegistrationData {
         return this;
     }
 
+    public RegistrationData withCurp(String curp) {
+        this.curp = curp;
+        return this;
+    }
+
+    public RegistrationData withRfc(String rfc) {
+        this.rfc = rfc;
+        return this;
+    }
+
+    public RegistrationData withFlat(String flat) {
+        this.flat = flat;
+        return this;
+    }
+
+    public RegistrationData withHouse(String house) {
+        this.house = house;
+        return this;
+    }
+
+    public RegistrationData withStreet(String street) {
+        this.street = street;
+        return this;
+    }
+
+    public RegistrationData withPostIndex(String postIndex) {
+        this.postIndex = postIndex;
+        return this;
+    }
+
+    public RegistrationData withColony(String colony) {
+        this.colony = colony;
+        return this;
+    }
+
+    public RegistrationData withState(String State) {
+        this.State = State;
+        return this;
+    }
+
+    public RegistrationData withDelegation(String delegation) {
+        this.delegation = delegation;
+        return this;
+    }
 
     public String getFirstName() {
         return firstName;
@@ -82,5 +138,40 @@ public class RegistrationData {
         return password;
     }
 
+    public String getCurp() {
+        return curp;
+    }
+
+    public String getRfc() {
+        return rfc;
+    }
+
+    public String getFlat() {
+        return flat;
+    }
+
+    public String getHouse() {
+        return house;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getPostIndex() {
+        return postIndex;
+    }
+
+    public String getColony() {
+        return colony;
+    }
+
+    public String getState() {
+        return State;
+    }
+
+    public String getDelegation() {
+        return delegation;
+    }
 }
 
