a50b378520573c06642958037a385cb3b064de6b
AksenovMSK <44177189+AksenovMSK@users.noreply.github.com>
2019-02-07 10:39:37 +0300

    убрал добавление не обязательной выписки из банка

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationForTariffHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationForTariffHelper.java
@@ -147,19 +147,19 @@ public class ApplicationForTariffHelper extends BaseHelper {
     }
 
     public void fillFormStep4(RegistrationData data) throws InterruptedException {
-        //открываем попап прикрепления документов
-        click(By.xpath("//*[@id=\"nojs\"]/body/div[2]/div/div[3]/div/div/div/div[1]/form/div/fieldset/div/a"));
-        //открываем выподающее меню для выбора типа прикрепляемого документа
-        click(By.xpath("//*[@id=\"senddocuments-form\"]/form/div[2]/div/div[1]/div/div[1]/span[1]/span[1]/span"));
-        //кликаем по элементу с указанным индексом
-        click(By.cssSelector("#select2-DocumentType-results li[role='treeitem']"), 1);
-        //вводим (добавляем) в поле прикрепляемый файл (документ)
-        attach(By.id("File"), data.getBankAccountStatement());
-        //прикрепляем файл
-        click(By.xpath("//*[@id=\"senddocuments-form\"]/form/div[2]/div/button"));
-        //закрываем попап удачного прикрепления документа
-        Thread.sleep(800);
-        click(By.xpath("//*[@id=\"senddocuments-form\"]/button"));
+//        //открываем попап прикрепления документов
+//        click(By.xpath("//*[@id=\"nojs\"]/body/div[2]/div/div[3]/div/div/div/div[1]/form/div/fieldset/div/a"));
+//        //открываем выподающее меню для выбора типа прикрепляемого документа
+//        click(By.xpath("//*[@id=\"senddocuments-form\"]/form/div[2]/div/div[1]/div/div[1]/span[1]/span[1]/span"));
+//        //кликаем по элементу с указанным индексом
+//        click(By.cssSelector("#select2-DocumentType-results li[role='treeitem']"), 1);
+//        //вводим (добавляем) в поле прикрепляемый файл (документ)
+//        attach(By.id("File"), data.getBankAccountStatement());
+//        //прикрепляем файл
+//        click(By.xpath("//*[@id=\"senddocuments-form\"]/form/div[2]/div/button"));
+//        //закрываем попап удачного прикрепления документа
+//        Thread.sleep(800);
+//        click(By.xpath("//*[@id=\"senddocuments-form\"]/button"));
 
         click(By.xpath("//*[@id=\"nojs\"]/body/div[2]/div/div[3]/div/div/div/div[1]/form/div/fieldset/div/a"));
         click(By.xpath("//*[@id=\"senddocuments-form\"]/form/div[2]/div/div[1]/div/div[1]/span[1]/span[1]/span"));
